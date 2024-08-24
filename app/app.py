e830c5cd194b75626e1ac13c5d833f3b7b6700ac
dj <darrellbrand@gmail.com>
2024-08-24 03:52:23 -0700

    wip

--- app/app.py
+++ app/app.py
@@ -1,14 +1,26 @@
 from flask import Flask
+import google.generativeai as genai
+import os
+from flask.cli import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.environ["API_KEY"])
+
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Flask Constructor
 app = Flask(__name__)
 
+
 # decorator to associate
 # a function with the url
-@app.route("/")
-def showHomePage():
-      # response from the server
-    return "This is home page"
+@app.route("/ai/<question>")
+def getAIResponse(question):
+    # response from the server
+    response = model.generate_content(question)
+    print(response.text)
+    return response.text
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
